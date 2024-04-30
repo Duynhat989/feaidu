c6b11cd4ddb5e2b1dc8ef2c69b507acf2d554ec5
duynhat989 <vietduy989kc@gmail.com>
2024-04-30 09:22:12 +0700

    back 1

--- src/components/chat/Room.vue
+++ src/components/chat/Room.vue
@@ -64,7 +64,7 @@ const sendEnter = (event) => {
 const sendMessage = async () => {
     var textMesg = textMessage.value
     console.log(textMesg.length)
-    if (textMesg.length < 3) {
+    if (textMesg.length < 2) {
         message.error('Mời bạn nhập nội dung!');
         return
     }
